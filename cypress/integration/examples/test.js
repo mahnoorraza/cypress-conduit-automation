48cbf1f0997b7842388019af5e6b43742d08440c
Conduit-Application-For-Automation-Practice <muzammilfarooqui111@gmail.com>
2021-06-26 18:48:04 +0500

    26-06-2021 Update test

--- cypress/integration/examples/test.js
+++ cypress/integration/examples/test.js
@@ -1,19 +1,21 @@
-// <reference types="Cypress" />
-
-
-describe('Verify Post Article Functionality', function(){
-
-
-    it("Signin with valid user", function()
-    {   
-        cy.visit("https://www.google.com")
+describe('Example to Demostrate the use of skip in cypress', () => 
+{
+    before(() => {
+        cy.visit('https://wikipedia.org')
         cy.wait(5000)
-        cy.switchToTab("Google")
     })
 
+    it.skip('Validate Page Title', () => {
+        cy.title().should('eq', 'Wikipedia')
+    })
+
+    it('Search for Google Wiki Page', () => {
+        cy.get('#searchInput').type('google')
+        cy.get('button[type="submit"]').click()
+    })
+
+    it.skip('Validate Google Wiki Page has opened', () => {
+        cy.get('h1#firstHeading').contains('Google')
+        cy.title().should('eq', 'Google - Wikipedia')
+    })
 })
-
-
-
-
-
